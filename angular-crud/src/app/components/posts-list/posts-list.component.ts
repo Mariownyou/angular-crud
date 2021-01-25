b9f773d84f92219a3b59e60f2da91cf8e35fd979
Leo Kondratev <39639000+Mariownyou@users.noreply.github.com>
2021-01-25 22:15:29 +0300

    все посты

--- angular-crud/src/app/components/posts-list/posts-list.component.ts
+++ angular-crud/src/app/components/posts-list/posts-list.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
+import { PostService } from 'src/app/services/post.service'
 
 @Component({
-  selector: 'app-posts-list',
+  selector: 'posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.scss']
 })
 export class PostsListComponent implements OnInit {
+  posts?: Post[];
+  currentPost?: Post;
+  currentIndex = -1;
+  title = '';
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.retrievePosts();
+  }
+
+  retrievePosts(): void {
+    this.postService.getAll()
+      .subscribe(
+        data => {
+          this.posts = data
+          console.log(data)
+        },
+        error => {
+          console.log(error)
+        })
+  }
+
+  refreshList(): void {
+    this.retrievePosts()
+    this.currentPost = undefined
+    this.currentIndex = -1
+  }
+
+  setActivePost(post: Post, index: number): void {
+    this.currentPost = post
+    this.currentIndex = index
+  }
+
+  removeAllPosts(): void {
+    this.postService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response)
+          this.refreshList()
+        },
+        error => {
+          console.log(error)
+        })
+  }
+
+  searchTitle(): void {
+    this.postService.findByTitle(this.title)
+      .subscribe(
+        data => {
+          this.posts = data
+          console.log(data)
+        },
+        error => {
+          console.log(error)
+        })
   }
 
 }
